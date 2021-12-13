02df335c8032130765739b786752745f3fb46245
Felype Nascimento <Felype.nasc@hotmail.com>
2021-12-13 02:49:33 -0300

    exercicio for each

--- pages/js-studies/12-for-each/script.js
+++ pages/js-studies/12-for-each/script.js
@@ -57,24 +57,39 @@ const carSpecs = {
 };
 
 const raceSpecs = [
-    {numberOfLaps: 10, firstPlace: 200, secondPlace: 120, thirdPlace: 50},
-    {numberOfLaps: 70, firstPlace: 220, secondPlace: 130, thirdPlace: 75},
-    {numberOfLaps: 160, firstPlace: 250, secondPlace: 150, thirdPlace: 90}
+    {
+        numberOfLaps: 10, 
+        firstPlace: 200, 
+        secondPlace: 120, 
+        thirdPlace: 50},
+    {
+        numberOfLaps: 70, 
+        firstPlace: 220, 
+        secondPlace: 130, 
+        thirdPlace: 75},
+    {
+        numberOfLaps: 160, 
+        firstPlace: 250, 
+        secondPlace: 150, 
+        thirdPlace: 90}
+];
+
+const playersInfo = [
+    {name: 'Pedro', lvl: 0, exp: 0},
+    {name: 'Juca', lvl: 0, exp: 0},
+    {name: 'Edna', lvl: 0, exp: 0}
 ];
 
 let numberOfLaps = '';
-let playersInfo = [
-    {name: pedro, lvl: 0, exp: 0},
-    {name: juca, lvl: 0, exp: 0},
-    {name: edna, lvl: 0, exp: 0}
-];
+let raceType = '';
+
 
 // base functions
 function getRandom(min, max) {
     return Math.random() * (max - min + 1) + min;
 };
 
-function randomizeCarType() {
+function randomizeCarType () {
     x = Math.random() * 100;
     if (x <= 60) return 'popular';
     if (x <= 95) return 'sport';
@@ -108,6 +123,7 @@ function changeToMainScreen () {
 };
 
 function changeToRaceScreen (option) {
+    raceType = option;
     numberOfLaps = raceSpecs[option].numberOfLaps;
     if (option == 0) {
         raceName.innerHTML = 'Corrida rápida (10 voltas)';
@@ -124,7 +140,7 @@ function changeToRaceScreen (option) {
     } else if (option == 3){
         mainScreen.style.display = "none";
         customRaceScreen.style.display = "block";
-    };
+    }
 };
 
 function customRaceCheckingLaps () {
@@ -135,71 +151,97 @@ function customRaceCheckingLaps () {
         raceScreen.style.display = "flex";
     } else {
         warningInsertMessage.style.display = "block";
-    };
+    }
 };
 
-function expCheck () {
-    playersInfo.forEach( (player) => {
+function expCheck(player) {
         if (player.exp >= 450 && player.lvl < 10) {
-            player.lvl += 1;
-            player.exp = player.exp % 450;
+        player.lvl += 1;
+        player.exp = player.exp % 450;
         }
-    });
 };
 
 // play race function
-
 function playRace() {
-    let pedroWins = 0;
-    let jucaWins = 0;
-    let ednaWins = 0;
+    let playersRanking = [
+        {name: 'Pedro', wins: 0, id: 0},
+        {name: 'Juca', wins: 0, id: 1},
+        {name: 'Edna', wins: 0, id: 2}
+    ];
     pedroCar = generateNewCar();
     jucaCar = generateNewCar();
     ednaCar = generateNewCar();
 
     // checking lap winner
     for (let i = 0; i < numberOfLaps; i++) {
-        let pedroCarVel = getRandom(pedroCar.minVel, pedroCar.maxVel)-pedroCar.drift;
-        let jucaCarVel = getRandom(jucaCar.minVel, jucaCar.maxVel)-jucaCar.drift;
-        let ednaCarVel = getRandom(ednaCar.minVel, ednaCar.maxVel)-ednaCar.drift;
+        let pedroCarVelTemp = getRandom(pedroCar.minVel, pedroCar.maxVel)-pedroCar.drift;
+        let pedroCarVel = pedroCarVelTemp - (pedroCarVelTemp*(playersInfo[0].lvl/100))
+        let jucaCarVelTemp = getRandom(jucaCar.minVel, jucaCar.maxVel)-jucaCar.drift;
+        let jucaCarVel = jucaCarVelTemp - (jucaCarVelTemp*(playersInfo[1].lvl/100))
+        let ednaCarVelTemp = getRandom(ednaCar.minVel, ednaCar.maxVel)-ednaCar.drift;
+        let ednaCarVel = ednaCarVelTemp - (ednaCarVelTemp*(playersInfo[2].lvl/100))
 
         if (pedroCarVel > jucaCarVel && pedroCarVel > ednaCarVel) {
-            pedroWins += 1;
-        } else if (jucaCarVel > ednaCarVel) {
-            jucaWins += 1;
-        } else {
-            ednaWins += 1;
+            playersRanking[0].wins += 1;
+        } else if (jucaCarVel > pedroCarVel && jucaCarVel > ednaCarVel) {
+            playersRanking[1].wins += 1;
+        } else if (ednaCarVel > pedroCarVel && ednaCarVel > jucaCarVel){
+            playersRanking[2].wins += 1;
         };
     };
 
     // outputing to html the race info
-    pedroWinsOutput.innerHTML = pedroWins+' Vitórias';
+    pedroWinsOutput.innerHTML = playersRanking[0].wins+' Vitórias';
     pedroContainer.classList.remove('winner');
     pedroCarModel.innerHTML = "Modelo: "+pedroCar.category;
     pedroCarModel.classList.remove('popular', 'sport', 'superSport');
     pedroCarModel.classList.add(pedroCar.category);
 
-    jucaWinsOutput.innerHTML = jucaWins+' Vitórias';
+    jucaWinsOutput.innerHTML = playersRanking[1].wins+' Vitórias';
     jucaContainer.classList.remove('winner');
     jucaCarModel.innerHTML = "Modelo: "+jucaCar.category;
     jucaCarModel.classList.remove('popular', 'sport', 'superSport');
     jucaCarModel.classList.add(jucaCar.category);
 
-    ednaWinsOutput.innerHTML = ednaWins+' Vitórias';
+    ednaWinsOutput.innerHTML = playersRanking[2].wins+' Vitórias';
     ednaContainer.classList.remove('winner');
     ednaCarModel.innerHTML = "Modelo: "+ednaCar.category;
     ednaCarModel.classList.remove('popular', 'sport', 'superSport');
     ednaCarModel.classList.add(ednaCar.category);
     
     // defining winner
-    if (pedroWins > jucaWins && pedroWins > ednaWins) {
-        winnerContainer.innerHTML = "PEDRO!";
-        pedroContainer.classList.add('winner')
-    } else if (jucaWins > ednaWins) {
-        winnerContainer.innerHTML = "JUCA!";
-        jucaContainer.classList.add('winner')
-    } else if (ednaWins > jucaWins){
-        winnerContainer.innerHTML = "EDNA!";
-        ednaContainer.classList.add('winner')
-    };
+    playersRanking.sort((a, b) => a.wins - b.wins);
+    console.log(playersRanking);
+
+    if (playersRanking[playersRanking.length - 1].wins == playersRanking[playersRanking.length - 2].wins) {
+        let player1 = generateNewCar();
+        let player2 = generateNewCar();
+        let player1CarVel = 0;
+        let player2CarVel = 1;
+        while (player1CarVel == player2CarVel) {
+            player1CarVel = getRandom(player1.minVel, player1.maxVel)-player1.drift;
+            player2CarVel = getRandom(player2.minVel, player2.maxVel)-player2.drift;
+        }
+        if (player1 > player2) {
+            playersRanking[playersRanking.length - 1].wins += 1;
+        } else {
+            playersRanking[playersRanking.length - 2].wins += 1;
+        }
+    }
+
+    winnerContainer.innerHTML = playersRanking[playersRanking.length - 1].name
+    if (playersRanking[playersRanking.length - 1].name == 'Pedro') {
+        pedroContainer.classList.add('winner');
+    } else if (playersRanking[playersRanking.length - 1].name == 'Juca') {
+        jucaContainer.classList.add('winner');
+    } else {
+        ednaContainer.classList.add('winner');
+    }
+    playersInfo[playersRanking[playersRanking.length-1].id].exp += raceSpecs[raceType].firstPlace
+    playersInfo[playersRanking[playersRanking.length-2].id].exp += raceSpecs[raceType].secondPlace
+    playersInfo[playersRanking[playersRanking.length-3].id].exp += raceSpecs[raceType].thirdPlace
+    playersInfo.forEach(expCheck)
+    document.getElementById('pedrolvl').innerHTML = playersInfo[0].lvl
+    document.getElementById('jucalvl').innerHTML = playersInfo[1].lvl
+    document.getElementById('ednalvl').innerHTML = playersInfo[2].lvl
 };
