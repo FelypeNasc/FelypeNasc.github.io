b40267e31011ab508ba8ebde6e571618cfd5b1cf
Felype Nascimento <Felype.nasc@hotmail.com>
2021-12-22 04:27:33 -0300

    done

--- pages/js-studies/16-callback/script.js
+++ pages/js-studies/16-callback/script.js
@@ -2,7 +2,8 @@ const startDraw = document.getElementById('startDraw');
 const numbersDisplay = document.getElementById('numbersContainer');
 
 const maxNum = 60;
-let drawnNumbers = [];
+const maxNumToDraw = 6;
+let numbersDrawn = [];
 
 function getRandom(min, max) {
     return Math.random() * (max - min + 1) + min;
@@ -10,14 +11,25 @@ function getRandom(min, max) {
 
 startDraw.addEventListener('click', drawNumbers)
 
-const drawNumbers = () => {
+function pushNum(num) {
+    numbersDrawn.push(num)
+}
+
+function drawNumbers() {
     numbersDisplay.innerHTML = '';
-    setInterval(() => {
-        if (!drawnNumbers.length === 6) {
-            let currNum = getRandom(1, 60)
+    numbersDrawn = []
+    let tmp = setInterval(() => {
+        let currNum = Math.ceil(getRandom(0, maxNum));
 
+        while (numbersDrawn.includes(currNum)) {
+            currNum = Math.ceil(getRandom(0, maxNum))
+        }
+
+        if (numbersDrawn.length !== maxNumToDraw) {
+            numbersDrawn.push(currNum)
+            numbersDisplay.innerHTML += `<div class=numbers>${currNum}</div>`
         } else {
-
+            clearInterval(tmp)
         }
     }, 1000)
-}+
+}
