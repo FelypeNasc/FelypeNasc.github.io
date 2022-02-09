feee41646a3af3827a45efb7422b085ef059619e
Felype Nascimento <Felype.nasc@hotmail.com>
2022-02-08 22:22:42 -0300

    style ok

--- pages/jquery-studies/02-fetch-moedas/script.js
+++ pages/jquery-studies/02-fetch-moedas/script.js
@@ -1,26 +1,36 @@
 $(document).ready(() => {
     $.ajax({url:"https://economia.awesomeapi.com.br/json/all"})
         .done((data) => {
+            $('#currency-select-from').append(`<option>BRL</option>`);
+            $('#currency-select-to').append(`<option selected>BRL</option>`);
             Object.entries(data).forEach((currency) => {
-                $('#currencySelect').append(`<option>${currency[0]}</option>`)
+                $('#currency-select-from').append(`<option>${currency[0]}</option>`);
+                $('#currency-select-to').append(`<option>${currency[0]}</option>`);
             })
+            $('#currency-select-from').val("USD");
+            $.ajax({ url: `https://economia.awesomeapi.com.br/json/USD-BRL` })
+                .done((data) => {
+                    const currencyFrom = JSON.stringify(data[0].code).replace(/"/g, '');
+                    const currencyTo = JSON.stringify(data[0].codein).replace(/"/g, '');
+                    const currencyToValue = parseFloat(JSON.stringify(data[0].bid).replace(/"/g, ''));
+                    console.log(currencyFrom, currencyTo, currencyToValue.toFixed(2).replace(".", ","))
+                    $('#code').html(` 1 ${currencyFrom}`);
+                    $('#codein').html(`${currencyToValue.toFixed(2).replace(".", ",")} ${currencyTo}`);
+                })
         })
 });
 
-$("#sendButton").on('click', () => {
-    const selectedCurrency = $('#currencySelect').find(":selected").text()
-    console.log(selectedCurrency)
-    $.ajax({ url: `https://economia.awesomeapi.com.br/json/${selectedCurrency}` })
+$("#send-button").on('click', () => {
+    const selectedCurrencyFrom = $('#currency-select-from').find(":selected").text();
+    const selectedCurrencyTo = $('#currency-select-to').find(":selected").text();
+    $.ajax({ url: `https://economia.awesomeapi.com.br/json/${selectedCurrencyFrom}-${selectedCurrencyTo}` })
         .done((data) => {
-            $('#result').html(`
-                Nome: ${JSON.stringify(data[0].name.split('/')[0])}
-                Valor agora: ${JSON.stringify(data[0].bid)}
-                `)
-                // Nome: ${JSON.stringify(data)}
-                // Nome: ${JSON.stringify(data)}
-                // Nome: ${JSON.stringify(data)}
-                // Nome: ${JSON.stringify(data)}
-                // Nome: ${JSON.stringify(data)}
-    })
+            const currencyFrom = JSON.stringify(data[0].code).replace(/"/g, '');
+            const currencyTo = JSON.stringify(data[0].codein).replace(/"/g, '');
+            const currencyToValue = parseFloat(JSON.stringify(data[0].bid).replace(/"/g, ''));
+            console.log(currencyFrom, currencyTo, currencyToValue.toFixed(2).replace(".", ","))
+            $('#code').html(` 1 ${currencyFrom}`);
+            $('#codein').html(`${currencyToValue.toFixed(2).replace(".", ",")} ${currencyTo}`);
+        })
 })
 
