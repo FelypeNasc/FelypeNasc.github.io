5131934c42781b1fa475d3280fe87c8993fb712b
Felype Nascimento <Felype.nasc@hotmail.com>
2022-02-09 13:23:52 -0300

    fetch ok

--- pages/jquery-studies/02-fetch-moedas/script.js
+++ pages/jquery-studies/02-fetch-moedas/script.js
@@ -1,36 +1,61 @@
+const apiURL = "https://economia.awesomeapi.com.br/json/"
+
 $(document).ready(() => {
-    $.ajax({url:"https://economia.awesomeapi.com.br/json/all"})
+    $.ajax({url:`${apiURL}all`})
         .done((data) => {
-            $('#currency-select-from').append(`<option>BRL</option>`);
-            $('#currency-select-to').append(`<option selected>BRL</option>`);
             Object.entries(data).forEach((currency) => {
                 $('#currency-select-from').append(`<option>${currency[0]}</option>`);
-                $('#currency-select-to').append(`<option>${currency[0]}</option>`);
             })
             $('#currency-select-from').val("USD");
-            $.ajax({ url: `https://economia.awesomeapi.com.br/json/USD-BRL` })
+            $.ajax({ url: `${apiURL}USD` })
                 .done((data) => {
                     const currencyFrom = JSON.stringify(data[0].code).replace(/"/g, '');
-                    const currencyTo = JSON.stringify(data[0].codein).replace(/"/g, '');
                     const currencyToValue = parseFloat(JSON.stringify(data[0].bid).replace(/"/g, ''));
-                    console.log(currencyFrom, currencyTo, currencyToValue.toFixed(2).replace(".", ","))
                     $('#code').html(` 1 ${currencyFrom}`);
-                    $('#codein').html(`${currencyToValue.toFixed(2).replace(".", ",")} ${currencyTo}`);
+                    $('#codein').html(`${currencyToValue.toFixed(2).replace(".", ",")} BRL`);
                 })
         })
 });
 
-$("#send-button").on('click', () => {
-    const selectedCurrencyFrom = $('#currency-select-from').find(":selected").text();
-    const selectedCurrencyTo = $('#currency-select-to').find(":selected").text();
-    $.ajax({ url: `https://economia.awesomeapi.com.br/json/${selectedCurrencyFrom}-${selectedCurrencyTo}` })
+$("#currency-select-from").on('change', () => {
+    const selectedCurrency = $('#currency-select-from').find(":selected").text();
+    $.ajax({ url: `${apiURL}${selectedCurrency}` })
         .done((data) => {
             const currencyFrom = JSON.stringify(data[0].code).replace(/"/g, '');
-            const currencyTo = JSON.stringify(data[0].codein).replace(/"/g, '');
             const currencyToValue = parseFloat(JSON.stringify(data[0].bid).replace(/"/g, ''));
-            console.log(currencyFrom, currencyTo, currencyToValue.toFixed(2).replace(".", ","))
             $('#code').html(` 1 ${currencyFrom}`);
-            $('#codein').html(`${currencyToValue.toFixed(2).replace(".", ",")} ${currencyTo}`);
+            $('#codein').html(`${currencyToValue.toFixed(2).replace(".", ",")} BRL`);
         })
 })
 
+$("#send-button").on('click', () => {
+    const selectedCurrency = $('#currency-select-from').find(":selected").text();
+    const initialDate = new Date($('#date-start').val());
+    const finalDate = new Date($('#date-final').val());
+    let currDate = initialDate
+        
+    while(currDate <= finalDate){
+        const initialDateParse = currDate.toISOString().split('T')[0].replaceAll('-','');       
+        $.ajax({url: `${apiURL}${selectedCurrency}?start_date=${initialDateParse}&end_date=${initialDateParse}`})
+        .done((data) => {
+            console.log(data)
+            
+        });
+        currDate.setDate(currDate.getDate() + 1)
+    }   
+})
+
+
+// function addToTable() {
+
+//     `
+//     <tr>
+//         <th>Moeda</th>
+//         <th>Cotação Dia</th>
+//         <th>Mínima</th>
+//         <th>Máxima</th>
+//         <th>Variação</th>
+//         <th>Data e hora</th>
+//     </tr>
+//     `   
+// }
