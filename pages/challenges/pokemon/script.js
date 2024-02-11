93d92585a27deafd9516504cfd177efd0d6edd6e
Felype Nascimento <felype.nasc@hotmail.com>
2024-02-11 20:50:59 -0300

    feat: add poke type filter | fetch improve | add all pokemons

--- pages/challenges/pokemon/script.js
+++ pages/challenges/pokemon/script.js
@@ -1,11 +1,14 @@
-import { buildCard } from "./builders.js";
+import { buildPage } from "./builders/index.js";
+import { buildCard } from "./builders/cards.js";
+import { formatPokeObj } from "./helpers.js";
 
 const searchInput = document.getElementById('search-input')
+buildPage();
 main();
 
-let pokemons;
+export let pokemons;
 
-async function main(filteredPokemons) {
+export async function main(filteredPokemons) {
   if (filteredPokemons) return createPokemonCards(filteredPokemons);
   pokemons = await fetchAndStorePokemons();
   createPokemonCards(pokemons);
@@ -21,16 +24,25 @@ searchInput.addEventListener('input', function () {
 async function fetchAndStorePokemons() {
   const pokemons = [];
   if (!localStorage.getItem('pokemons')) {
-    for (let i = 1; i <= 151; i++) {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const requests = [];
+    for (let i = 1; i <= 1025; i++) {
+      requests.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`));
+    }
+
+    const responses = await Promise.all(requests);
+
+    for (const response of responses) {
       if (response.ok) {
         const pokemonData = await response.json();
         pokemons.push(pokemonData);
       } else {
-        console.error(`Erro ao buscar o pokemon ${i}:${response.status}`);
+        const pokemonNumber = response.url.match(/\/(\d+)\/$/)[1];
+        console.error(`Erro ao buscar o pokemon ${pokemonNumber}:${response.status}`);
       }
     }
-    const pokemonFilteredData = pokemons.map((poke) => formatPokeObj(poke))
+
+    const pokemonFilteredData = pokemons.map((poke) => formatPokeObj(poke));
+    console.log(pokemonFilteredData)
     localStorage.setItem('pokemons', JSON.stringify(pokemonFilteredData));
   }
   return JSON.parse(localStorage.getItem('pokemons'));
@@ -43,6 +55,13 @@ function createPokemonCards(pokemons) {
   pokemons.forEach(pokemon => {
     container.appendChild(buildCard(pokemon))
   });
+
+  if (pokemons.length < 1) {
+    const noPokeFound = document.createElement('h2');
+    noPokeFound.textContent = 'No Pokemon were found :(';
+
+    container.appendChild(noPokeFound)
+  }
 }
 
 function searchPokemons(input, pokemonArray) {
